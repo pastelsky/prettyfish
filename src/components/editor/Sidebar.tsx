a352bddbf87424745bee38ae2b7b1f7a7b958da1
Shubham Kanodia <shubham.kanodia10@gmail.com>
2026-04-16 08:26:33 +0530

    style: editor font size → 14px

--- src/components/editor/Sidebar.tsx
+++ src/components/editor/Sidebar.tsx
@@ -382,7 +382,7 @@ export function Sidebar({
                 height="100%"
                 style={{
                   height: '100%',
-                  fontSize: '13.5px',
+                  fontSize: '14px',
                   lineHeight: '1.7',
                   paddingTop: '6px',
                   fontVariantLigatures: editorLigatures ? 'common-ligatures' : 'none',
